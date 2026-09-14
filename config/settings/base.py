83622ab7a2241a337babf3e167fd1c5896a5ec58
Bhawani Singh <96902525+bhawanirathore7773@users.noreply.github.com>
2026-09-14 19:41:32 +0530

    fix: safely gate optional debug toolbar

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,3 +45,7 @@ LANGUAGE_CODE="en-us"
 TIME_ZONE="Asia/Kolkata"
 LEAD_NOTIFICATION_EMAIL=LEAD_NOTIFICATION_EMAIL
 DEFAULT_FROM_EMAIL=DEFAULT_FROM_EMAIL
+
+if DEBUG and "debug_toolbar" in INSTALLED_APPS:
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
